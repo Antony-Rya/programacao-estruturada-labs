b1bd3fe89397548690f2c9c5f831a9d40849987c
Antony-Rya <159479364+Antony-Rya@users.noreply.github.com>
2024-02-22 19:40:02 +0000

    questao 9

--- semana02-aula02/Q09.py
+++ semana02-aula02/Q09.py
@@ -1,7 +1,10 @@
 #Escreva um programa que solicite ao usuário dois valores booleanos (True ou False) e armazene-os em variáveis. Em seguida, aplique os operadores lógicos "and", "or" e "not" entre essas variáveis e imprima os resultados.
-E1 = bool(input("Digite um valor booleano: "))
-E2 = bool(input("Digite outro valor booleano: "))
+E1 = int(input("Digite um valor booleano: "))
+E1_bool = bool(E1) 
+E2 = int(input("Digite outro valor booleano: "))
+E2_bool = bool(E2) 
 
-print("Os dois valores passando pelo operador AND retorna: ", E1 and E2)
-print("Os dois valores passando pelo operador OR retorna: ", E1 or E2)
-print("Os dois valores passando pelo operador NOT retorna: ", E1 and E2)
+print("Os dois valores passando pelo operador AND retorna: ", E1_bool and E2_bool)
+print("Os dois valores passando pelo operador OR retorna: ", E1_bool or E2_bool)
+print("O valor 1 passando pelo operador NOT retorna: ", not E1_bool)
+print("O valor 2 passando pelo operador NOT retorna: ", not E2_bool)
