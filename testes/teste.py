48686aed6c6a58f11f30b8b03c6355eab6d633e0
Antony-Rya <159479364+Antony-Rya@users.noreply.github.com>
2024-07-11 19:56:30 +0000

    salvando

--- testes/teste.py
+++ testes/teste.py
@@ -1,5 +1,17 @@
-frutas = ['maçã', 'banana', 'laranja']
-cores = ['vermelho', 'amarelo', 'laranja']
+soma = 0
+n1 = int(input())
+n2 = int(input())
+if n1>n2:
+    primeiro = n2
+    segundo = n1
+else:
+    primeiro = n1
+    segundo = n2
 
-for fruta, cor in zip(frutas, cores):
-    print(f"{fruta} é da cor {cor}")
+
+primeiro = n2, segundo = n1 if (n1 > n2) else n1,  n2
+    
+for i in range(primeiro, segundo + 1):
+    if i > 0:
+        soma += i
+print(soma)
