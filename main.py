7e8bbc9f6f4a6f72dd88c574b127a05226eb1a04
Antony-Rya <159479364+Antony-Rya@users.noreply.github.com>
2024-08-10 14:54:05 +0000

    11

--- main.py
+++ main.py
@@ -46,3 +46,52 @@ def idade_valida(idade):
         return True
     else:
         return False
+    
+def validar_email(email):
+    '''
+    8-Implemente uma função validar_email que verifica se uma 
+    string fornecida como argumento representa um endereço de 
+    e-mail válido. Considere que um email válido não deve ter 
+    espaços, deve conter 01 arroba e terminar com .com
+    '''
+    # if "@" not in email and ".com" not in email and " " not in email:
+    #     if email.indexOf("@" + 1) == ".":
+    #         return False
+    # else:
+    #     return True
+
+    if email.find("@") and email.find(".com") and (email.find(" ") == False):
+        if email.find("@.") != -1:
+          return True
+    else:
+        return False
+
+def calcular_pagamento(horas_trabalhadas, taxa_hora):
+    '''
+    Escreva uma função calcular_pagamento que 
+    aceita os parâmetros nomeados horas_trabalhadas 
+    e taxa_hora e retorna o pagamento total.
+    '''
+    return horas_trabalhadas * taxa_hora
+
+def maior_numero(a, b, c):
+    '''
+    Crie uma função que retorne o maior número dentre 3 elementos.
+    '''
+    return max(a, b , c)
+
+def contagem_letras(string):
+    '''
+    Escreva uma função em Python function que aceita uma string e 
+    retorna a quantidade de letras maiúsculas e minúsculas.
+    '''
+    maisculas = 0
+    minusculas = 0
+    resultado = ()
+    for letra in string:
+        if letra.isupper():
+            maisculas += 1
+        else:
+            minusculas += 1
+    resultado = (maisculas, minusculas)
+    return resultado
