1ed09c6e50081c64b2ddcecdd04cdb211185611b
Antony-Rya <159479364+Antony-Rya@users.noreply.github.com>
2024-08-30 18:57:48 +0000

    1

--- semanaAgo-aula???/Q10.py
+++ semanaAgo-aula???/Q10.py
@@ -12,10 +12,24 @@ Input: head = [1,1,2]
 Output: [1,2]
 
 '''
-lista = sorted(gerar_lancamentos(1, 10, 10))
+# lista = sorted(gerar_lancamentos(1, 10, 10))
+# print(lista)
+# for i, k in enumerate(lista):
+#     if lista.count(k) > 1:
+#         lista.pop(i)
+# nova_lista = sorted(lista)
+# print(nova_lista)
+
+lista = sorted(gerar_lancamentos(0, 10, 10))
 print(lista)
-for i, k in enumerate(lista):
-    if lista.count(k) > 1:
+sucessor = 0
+antecessor = 0
+for i in range(len(lista)):
+    if i > len(lista):
+        sucessor = lista[i +1]
+        
+    if i > 0:
+        antecessor = lista[i - 1] 
+    if i == sucessor:
         lista.pop(i)
-nova_lista = sorted(lista)
-print(nova_lista)
+print(lista)
